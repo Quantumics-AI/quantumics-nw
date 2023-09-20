1a9ed8898e0f7239c0b7bb2fc06d3039361b65f4
rahul <rahul.borse@quantumics.ai>
2023-09-20 18:19:59 +0530

    common service changes

--- services/qs-common-service/src/main/java/com/qs/api/AwsCustomConfiguration.java
+++ services/qs-common-service/src/main/java/com/qs/api/AwsCustomConfiguration.java
@@ -1,9 +1,11 @@
 package com.qs.api;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+@Configuration
 public class AwsCustomConfiguration {
     @Bean
     RestTemplate getRestTemplate() {
