46afc5da6b0719f01266b101552161c8f32a585b
Shivraj <shivraj.mohite@quantumics.ai>
2023-10-12 16:19:05 +0530

    fixed the functionality page pagination

--- ui/src/app/pages/data-quality/components/data-quality-list/data-quality-list.component.ts
+++ ui/src/app/pages/data-quality/components/data-quality-list/data-quality-list.component.ts
@@ -43,6 +43,7 @@ export class DataQualityListComponent implements OnInit {
   public endIndex: number = this.pageSize;
   public page = 1;
   public rulesData: any;
+  public paginationData: any;
 
   public ruleStatus: string = 'Active';
   public pageNumebr: number = 1;
@@ -83,6 +84,7 @@ export class DataQualityListComponent implements OnInit {
       this.loading = false;
       this.dataQualityList = response?.result?.content;
       this.ruleCount =  response?.result?.content;
+      this.paginationData = response?.result;
 
       if (this.dataQualityList.length > 1) {
         this.dataQualityList.sort((val1, val2) => {
@@ -235,6 +237,7 @@ export class DataQualityListComponent implements OnInit {
     this.ruleCreationService.getRulesData(this.userId, this.projectId, this.ruleStatus, this.pageNumebr, this.pageLength).subscribe((response) => {
       
       this.dataQualityList = response?.result?.content;
+      this.paginationData = response?.result;
       this.loading = false;
     }, (error) => {
       this.loading = false;
@@ -244,6 +247,14 @@ export class DataQualityListComponent implements OnInit {
 
   // pagination 
   public onPageChange(currentPage: number): void {
+    debugger
+    if(this.pageNumebr < currentPage){
+      this.pageNumebr = currentPage;
+    } else {
+      this.pageNumebr = this.pageNumebr - currentPage;
+    }
+    this.getRules();
+    // this.pageNumebr = 
     this.startIndex = (currentPage - 1) * this.pageSize;
     this.endIndex = this.startIndex + this.pageSize;
   }
